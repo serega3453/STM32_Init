ea039d851daa887b3f51a9a98d69def37126fd5c
Smalec <sergiy.larchenko@i.ua>
2025-10-23 15:53:20 +0300

    Init2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,9 @@
 #define RCC_BASE 0x40021000U
 #define GPIOA_BASE 0x48000000U
 #define GPIOB_BASE 0x48000400U
+#define GPIOF_BASE 0x48001400U
 #define TIM3_BASE 0x40000400U
+#define I2C1_BASE 0x40005400U
 
 #define RCC_AHBENR vol(RCC_BASE + 0x14U)
 #define RCC_APB1ENR vol(RCC_BASE + 0x1CU)
@@ -24,6 +26,12 @@
 #define GPIOB_PUPDR vol(GPIOB_BASE + 0x0CU)
 #define GPIOB_AFRL vol(GPIOB_BASE + 0x20U)
 
+#define GPIOF_MODER vol(GPIOF_BASE + 0x00U)
+#define GPIOF_OTYPER vol(GPIOF_BASE + 0x04U)
+#define GPIOF_OSPEEDR vol(GPIOF_BASE + 0x08U)
+#define GPIOF_PUPDR vol(GPIOF_BASE + 0x0CU)
+#define GPIOF_AFRL vol(GPIOF_BASE + 0x20U)
+
 #define TIM3_CR1 vol(TIM3_BASE + 0x00U)
 #define TIM3_EGR vol(TIM3_BASE + 0x14U)
 #define TIM3_CCMR1 vol(TIM3_BASE + 0x18U)
@@ -36,14 +44,19 @@
 #define TIM3_CCR2 vol(TIM3_BASE + 0x38U)
 #define TIM3_CCR4 vol(TIM3_BASE + 0x40U)
 
+#define I2C1_CR1 vol(I2C1_BASE + 0x00U)
+
 unsigned char flag = 0b00000000;
 uint32_t color = 0x08U;
 
 void RCC_EnableClock()
 {
     write_bits(&RCC_APB1ENR, (0x01U << 1), (0x01U << 1)); //TIM3_EN
+    write_bits(&RCC_APB1ENR, (0x01U << 21), (0x01U << 21)); //I2C1_EN
     raw_delay(10000);
     write_bits(&RCC_AHBENR, (0x03U << 17), 0x03U << 17); //GPIOA_EN && GPIOB_EN
+    write_bits(&RCC_AHBENR, (0x01U << 22), (0x01U << 22)); //GPIOF_EN
+
     raw_delay(10000);
 
 }
@@ -59,11 +72,20 @@ void GPIOA_Config()
 
 void GPIOB_Config()
 {
-    write_bits(&GPIOB_MODER, (0x03U << 2), (0x2U << 2));
+    write_bits(&GPIOB_MODER, (0x03U << 2), (0x02U << 2));
     write_bits(&GPIOB_OTYPER, (0x01U << 1), (0x00U << 1));
     write_bits(&GPIOB_OSPEEDR, (0x03U << 2), (0x03U << 2));
     write_bits(&GPIOB_PUPDR, (0x03U << 2), (0x00U << 2));
     write_bits(&GPIOB_AFRL, (0x0FU << 4), (0x01U << 4));
+}
+
+void GPIOF_Config()
+{
+    write_bits(&GPIOF_MODER, (0x0FU << 0), (0x0AU << 0));
+    write_bits(&GPIOF_OTYPER, (0x03U << 0), (0x03U << 0));
+    write_bits(&GPIOF_OSPEEDR, (0x0FU << 0), (0x0FU << 0));
+    write_bits(&GPIOB_PUPDR, (0x0FU << 0), (0xA0U << 0));
+    write_bits(&GPIOB_AFRL, (0xFFU << 0), (0x11U << 0));
 }
 
 void TIM3_Config()
@@ -81,6 +103,11 @@ void TIM3_Config()
     write_reg(&TIM3_CCR4, 0x00000000);
     write_reg(&TIM3_EGR, 0x00000001);
     write_bits(&TIM3_CR1, (0x01U << 0), (0x01U << 0));
+}
+
+I2C1_Config()
+{
+    write_bits(I2C1_CR1, (0x01U << 0), (0x00U << 0));
 }
 
 uint32_t Next_Color(uint32_t* col)
